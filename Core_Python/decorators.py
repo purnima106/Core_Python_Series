6de1c1faeaf798994e7c9da288af80debb054278
Purnimanetworkscience <purnimanahata106@gmail.com>
2026-03-28 23:35:03 +0530

    phase 2 dec

--- Core_Python/decorators.py
+++ Core_Python/decorators.py
@@ -1,11 +1,18 @@
 import time
 
-def timer(func):
-    def wrapper(*args, **kwargs):
-        start = time.timer()
+def timer(func):#function and another func
+    def wrapper(*args, **kwargs):#inner func
+        start = time.time() 
         result = func(*args, **kwargs)
         end = time.time()
         print(f"[timer] {func.__name__} took {end - start:.4f}s")
         return result
     return wrapper
+
+@timer
+def train_model(epochs): #Define function with parameter epochs
+    time.sleep(0.5)#Pause for 0.5 seconds (simulate work)
+    print(f"Model trained for {epochs} epochs")
+
+train_model(10)
 
