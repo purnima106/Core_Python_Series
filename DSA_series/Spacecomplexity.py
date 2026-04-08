2c246edfdaa3c180ec3e60c57064a99d92c828a7
Purnima Nahata <purnimanahata106@gmail.com>
2026-04-08 23:45:23 +0530

    Space Complexity recursion

--- DSA_series/Spacecomplexity.py
+++ DSA_series/Spacecomplexity.py
@@ -8,3 +8,24 @@ for x in arr:
     total += x
     
 print(total)
+
+#eg 2: O(n)
+
+arr = [1,2,3,4,5]
+
+new_arr = []
+
+for x in arr:
+    new_arr.append(x)
+
+print(new_arr)
+
+#eg 3: Recursion (stack memory)
+
+def func(n):
+    if n == 0:
+        return
+    print(n)
+    func(n - 1)
+
+func(3)
