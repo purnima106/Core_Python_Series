6b06c79ad58fe1d7fed64e5019fd79f130ddf7b9
Purnima Nahata <purnimanahata106@gmail.com>
2026-04-09 11:16:40 +0530

    O(1) and O(n) Space Complexities understanding

--- DSA_series/Spacecomplexity.py
+++ DSA_series/Spacecomplexity.py
@@ -1,31 +1,57 @@
-#O(1) space
+# #O(1) space
 
-arr = [1,2,3,4,5]
+# arr = [1,2,3,4,5]
 
-total = 0
+# total = 0
 
-for x in arr:
-    total += x
+# for x in arr:
+#     total += x
     
-print(total)
+# print(total)
 
-#eg 2: O(n)
+# #eg 2: O(n)
 
-arr = [1,2,3,4,5]
+# arr = [1,2,3,4,5]
 
-new_arr = []
+# new_arr = []
 
-for x in arr:
-    new_arr.append(x)
+# for x in arr:
+#     new_arr.append(x)
 
-print(new_arr)
+# print(new_arr)
 
-#eg 3: Recursion (stack memory)
+# #eg 3: Recursion (stack memory)
 
-def func(n):
-    if n == 0:
+# def func(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     func(n - 1)
+
+# func(3)
+
+#eg 4: Space vs Time
+
+nums = [1,2,3,4,5]
+
+print(nums)
+print(nums[-2:])
+
+def func(arr):
+    if len(arr) == 0:
         return
-    print(n)
-    func(n - 1)
+    print(arr[0])
+    print(arr[1:])
 
-func(3)
+func(nums)
+
+x = nums[0]
+y = nums[1]
+z = nums[2]
+
+print(x,y,z)
+
+
+
+
+
