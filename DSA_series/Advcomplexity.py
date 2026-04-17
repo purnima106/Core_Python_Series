a3a9dac7b9badacd8534d553b0ece61633999c65
Purnima Nahata <purnimanahata106@gmail.com>
2026-04-17 22:56:49 +0530

    Two sum again

--- DSA_series/Advcomplexity.py
+++ DSA_series/Advcomplexity.py
@@ -1,27 +1,42 @@
-#1. Factorial — Linear Recursion (O(n))
+# #1. Factorial — Linear Recursion (O(n))
 
-def fact(n):
-    if n == 0:
-        return 1
-        #both tines are---base lines(stopping condition) prevents from infinite recusrion
-    return n * fact(n - 1)
+# def fact(n):
+#     if n == 0:
+#         return 1
+#         #both tines are---base lines(stopping condition) prevents from infinite recusrion
+#     return n * fact(n - 1)
 
-n = 4
-result = fact(n)
+# n = 4
+# result = fact(n)
 
-print("Factorial of", n, "is:", result)
+# print("Factorial of", n, "is:", result)
 
-#time comp...is O(n)--4 → 3 → 2 → 1 → 0---Total = 5 calls ≈ n
+# #time comp...is O(n)--4 → 3 → 2 → 1 → 0---Total = 5 calls ≈ n
 
 
-#2. Fibonacci
+# #2. Fibonacci
 
-def fib(n):
-    if n <= 1:
-        return n
-    return fib(n-1) + fib(n-2)
+# def fib(n):
+#     if n <= 1:
+#         return n
+#     return fib(n-1) + fib(n-2)
 
-n = 5
-for i in range(n):
-    print(f"fib({i}) =", fib(i))
+# n = 5
+# for i in range(n):
+#     print(f"fib({i}) =", fib(i))
+
+
+#rewriting the ques again
+
+nums = [2, 7]
+target = 9
+
+seen = {}
+
+for i in range(len(nums)):
+    num = nums[i]
+    needed = target - num
+
+    print("num:", num, "needed:", needed)
+
 
