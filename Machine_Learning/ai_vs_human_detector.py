a23553c5718f585540dfcc6aeb0efe8021b635f8
Purnima Nahata <purnimanahata106@gmail.com>
2026-05-10 23:56:20 +0530

    DATA INSPECTION + AUDITING

--- Machine_Learning/ai_vs_human_detector.py
+++ Machine_Learning/ai_vs_human_detector.py
@@ -14,3 +14,35 @@ print(df.shape)
 
 print("\n DATA TYPES:\n")
 print(df.dtypes)
+
+#Phase 2-DATA INSPECTION + AUDITING
+
+print("\n Missing Values: \n")
+print(df.isnull().sum())
+
+print("\n Duplicate Values: \n")
+print(df.duplicated().sum())
+duplicate_count = df.duplicated().sum()
+print(f"\n Duplicate count is:{duplicate_count}")
+
+print("\n Human VS AI Distribution: \n")
+print(df['human_or_ai'].value_counts())
+
+print("\n SAMPLE TEXT DATA:\n")
+
+for i in range(5):
+    print(f"\nROW {i+1}:")
+    print(df['text'].iloc[i])
+
+print("\n DATASET STATISTICS:\n")
+print(df.describe(include='all'))
+
+print("\n LANGUAGES PRESENT:\n")
+print(df['language'].unique())
+
+print("\n DOMAINS PRESENT:\n")
+print(df['domain'].unique())
+
+
+
+
